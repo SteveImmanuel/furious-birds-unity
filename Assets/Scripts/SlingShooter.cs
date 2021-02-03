d49e9433800100ff96458fbc53b8c59995ee7365
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-03 11:13:08 +0700

    Implement elactic rope on slingshot

--- Assets/Scripts/SlingShooter.cs
+++ Assets/Scripts/SlingShooter.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class SlingShooter : MonoBehaviour
 {
-    public LineRenderer trajectory;
-    public int trajectorySegmentCount = 5;
+    public LineRenderer aimTrajectory;
+    public LineRenderer ropeFront;
+    public LineRenderer ropeBack;
+    public int aimTrajectorySegmentCount = 5;
     [SerializeField]
     private float radius = 0.75f;
     [SerializeField]
@@ -15,6 +17,8 @@ public class SlingShooter : MonoBehaviour
     private Vector2 startPos;
     private CircleCollider2D col;
     private Bird bird;
+    private Vector3 ropeFrontIdlePos;
+    private Vector3 ropeBackIdlePos;
 
     private void Awake()
     {
@@ -24,6 +28,8 @@ public class SlingShooter : MonoBehaviour
     void Start()
     {
         startPos = transform.position;
+        ropeFrontIdlePos = ropeFront.GetPosition(0);
+        ropeBackIdlePos = ropeBack.GetPosition(0);
     }
 
     public void InitiateBird(Bird newBird)
@@ -42,7 +48,9 @@ public class SlingShooter : MonoBehaviour
         bird.Shoot(direction, distance, throwSpeed);
 
         gameObject.transform.position = startPos;
-        trajectory.enabled = false;
+        aimTrajectory.enabled = false;
+        ropeFront.SetPosition(0, ropeFrontIdlePos);
+        ropeBack.SetPosition(0, ropeBackIdlePos);
     }
 
     void OnMouseDrag()
@@ -63,10 +71,15 @@ public class SlingShooter : MonoBehaviour
         transform.position = startPos + dir;
         float distance = Vector2.Distance(startPos, transform.position);
 
-        if (!trajectory.enabled)
+        if (!aimTrajectory.enabled)
         {
-            trajectory.enabled = true;
+            aimTrajectory.enabled = true;
         }
+
+        Vector2 direction = (transform.position - ropeFront.GetPosition(1));
+        Vector2 newPosition = (Vector2)transform.position + direction.normalized * col.radius;
+        ropeFront.SetPosition(0, newPosition);
+        ropeBack.SetPosition(0, newPosition);
 
         DisplayTrajectory(distance);
     }
@@ -74,20 +87,20 @@ public class SlingShooter : MonoBehaviour
     void DisplayTrajectory(float distance)
     {
         Vector2 direction = (startPos - (Vector2)transform.position).normalized;
-        Vector2[] segments = new Vector2[trajectorySegmentCount];
+        Vector2[] segments = new Vector2[aimTrajectorySegmentCount];
         Vector2 shootVelocity = direction * throwSpeed * distance;
 
         segments[0] = transform.position;
-        for (int i = 1; i < trajectorySegmentCount; i++)
+        for (int i = 1; i < aimTrajectorySegmentCount; i++)
         {
             float elapsedTime = i * Time.fixedDeltaTime * 20 / shootVelocity.magnitude; //make elapsed time inversely proportional with speed
             segments[i] = segments[0] + shootVelocity * elapsedTime + 0.5f * Physics2D.gravity * Mathf.Pow(elapsedTime, 2);
         }
 
-        trajectory.positionCount = trajectorySegmentCount;
-        for (int i = 0; i < trajectorySegmentCount; i++)
+        aimTrajectory.positionCount = aimTrajectorySegmentCount;
+        for (int i = 0; i < aimTrajectorySegmentCount; i++)
         {
-            trajectory.SetPosition(i, segments[i]);
+            aimTrajectory.SetPosition(i, segments[i]);
         }
     }
 }
