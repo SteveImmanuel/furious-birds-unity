a6a0069607cf303ae15f55a26862515f76919c32
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-03 16:20:55 +0700

    Add smoke effect upon die

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
     public float health = 50f;
 
     private GameController gameController;
+    private GameObject smokeParticle;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private CircleCollider2D col;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+        smokeParticle = transform.GetChild(0).gameObject;
+        spriteRenderer = GetComponent<SpriteRenderer>();
         gameController = GameController.instance;
     }
     private void OnDestroy()
@@ -28,8 +39,19 @@ public class Enemy : MonoBehaviour
 
             if (health <= 0)
             {
-                Destroy(gameObject);
+                StartCoroutine(DestroyAfter());
             }
         }
+    }
+
+    protected IEnumerator DestroyAfter()
+    {
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = Vector3.zero;
+        col.enabled = false;
+        smokeParticle.SetActive(true);
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
